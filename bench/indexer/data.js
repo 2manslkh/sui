aeff560a7c86462259f0abf29e33ffb85d5fae7f
github-action-benchmark <github@users.noreply.github.com>
2024-01-31 05:25:19 +0000

    add Benchmark (cargo) benchmark result for 2677541836f2b79bdcc9531a13053b9000f3cf36

--- bench/indexer/data.js
+++ bench/indexer/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1706667865014,
+  "lastUpdate": 1706678718755,
   "repoUrl": "https://github.com/MystenLabs/sui",
   "entries": {
     "Benchmark": [
@@ -29,6 +29,36 @@ window.BENCHMARK_DATA = {
             "name": "get_checkpoint",
             "value": 357486,
             "range": "± 25545",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "rvantonder@gmail.com",
+            "name": "Rijnard van Tonder",
+            "username": "rvantonder"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2677541836f2b79bdcc9531a13053b9000f3cf36",
+          "message": "move: use source package layout to resolve Move.lock path (#16017)\n\n## Description \r\n\r\nTidying up some lock file logic and spotted this\r\n\r\n## Test Plan \r\n\r\nCovered by existing tests\r\n\r\n---\r\nIf your changes are not user-facing and do not break anything, you can\r\nskip the following section. Otherwise, please briefly describe what has\r\nchanged under the Release Notes section.\r\n\r\n### Type of Change (Check all that apply)\r\n\r\n- [ ] protocol change\r\n- [ ] user-visible impact\r\n- [ ] breaking change for a client SDKs\r\n- [ ] breaking change for FNs (FN binary must upgrade)\r\n- [ ] breaking change for validators or node operators (must upgrade\r\nbinaries)\r\n- [ ] breaking change for on-chain data layout\r\n- [ ] necessitate either a data wipe or data migration\r\n\r\n### Release notes",
+          "timestamp": "2024-01-30T21:18:01-08:00",
+          "tree_id": "d9f7a9c0eed473d5dc029f126c8bb301acc29cad",
+          "url": "https://github.com/MystenLabs/sui/commit/2677541836f2b79bdcc9531a13053b9000f3cf36"
+        },
+        "date": 1706678714660,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "get_checkpoint",
+            "value": 315316,
+            "range": "± 25612",
             "unit": "ns/iter"
           }
         ]
